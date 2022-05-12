a9cbbd9705ba446ccb41944f76fdf6e86ce543fe
aligirisen <agirisen0@gmail.com>
2022-05-12 23:03:59 +0300

    s

---
+++ People.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Telephone_Directory_Application
+{
+    public class People
+    {
+        public List<Person> persons = new List<Person>();
+
+        public void people()
+        {
+            persons.Add(new Person("Ahmed", "Kuna", "05431564"));
+            persons.Add(new Person("Ali", "fersah", "054784561"));
+            persons.Add(new Person("Kemal", "Ridaniyeli", "056124878"));
+            persons.Add(new Person("Murat", "fıstıkçı", "053154678"));
+            persons.Add(new Person("Mehmet", "kayıkçı", "0584653"));
+        }
+
+    }
+}
